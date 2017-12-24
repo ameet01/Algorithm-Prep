c7a0d5d83b81aee9a821a28a2dc2c41bbce06423
Ameet Vadhia <ameet01@gmail.com>
2017-12-23 22:04:04 -0800

    5.1

---
+++ CH5/5.1.rb
@@ -0,0 +1,67 @@
+#Brute Force:
+# def dutch(array, pivot_index)
+#   smaller = array.select{|i| i < array[pivot_index]}
+#   equal = array.select{|i| i == array[pivot_index]}
+#   larger = array.select{|i| i > array[pivot_index]}
+  
+#   (smaller + equal + larger)
+# end
+
+# dutch([0,1,2,0,2,1,1], 2)
+
+#O(n)
+# def dutch(array, pivot_index)
+#   pivot = array[pivot_index]
+  
+#   smaller = 0
+  
+#   (0...array.length).each do |i|
+#     if array[smaller] < pivot
+#       array[i], array[smaller] = array[smaller], array[i]
+#       smaller += 1
+#     end
+#   end
+  
+#   larger = array.length - 1
+  
+#   (array.length-1).downto(0).each do |i|
+#     break if array[i] < pivot
+    
+#     if array[i] > pivot
+#       array[i], array[larger] = array[larger], array[i]
+#       larger -= 1
+#     end
+#   end
+  
+#   array
+# end
+
+# dutch([0,1,2,0,2,1,1], 3)
+
+
+def dutch(array, pivot_index)
+    pivot = array[pivot_index]
+    
+    pointer = 0
+    smaller = 0
+    larger = array.length - 1
+    
+    while pointer < larger
+      if array[pointer] < pivot
+        array[smaller], array[pointer] = array[pointer], array[smaller]
+        smaller += 1
+        pointer += 1
+      elsif array[pointer] == pivot
+        pointer += 1
+      elsif array[pointer] > pivot
+        array[larger], array[pointer] = array[pointer], array[larger]
+        larger -= 1
+      end
+    end
+    
+    array
+  end
+  
+  dutch([0,1,2,0,2,1,1], 3)
+  
+  
