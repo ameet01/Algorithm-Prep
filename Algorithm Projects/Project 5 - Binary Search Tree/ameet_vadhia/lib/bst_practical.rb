596ed8f9d494386bf3097b2d7a572d001ae31eac
Ameet Vadhia <ameet01@gmail.com>
2018-01-07 01:17:38 -0800

    Finish bst practical question

--- Algorithm Projects/Project 5 - Binary Search Tree/ameet_vadhia/lib/bst_practical.rb
+++ Algorithm Projects/Project 5 - Binary Search Tree/ameet_vadhia/lib/bst_practical.rb
@@ -1,3 +1,9 @@
 
+require_relative 'binary_search_tree'
+
 def kth_largest(tree_node, k)
+  bst = BinarySearchTree.new
+  bst.root = tree_node
+  array = bst.in_order_traversal
+  return bst.find(array[array.length - k])
 end
