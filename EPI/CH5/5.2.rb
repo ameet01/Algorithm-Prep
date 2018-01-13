ac4cff9e34d2680b2ef64b713c953cfce942a418
Ameet Vadhia <ameet01@gmail.com>
2018-01-13 15:07:34 -0800

    more algorithms

--- EPI/CH5/5.2.rb
+++ EPI/CH5/5.2.rb
@@ -4,13 +4,18 @@
 #   (array.join.to_i + 1).to_s.split('').map(&:to_i)
 # end
 
+
 def plus_one(array)
   array[-1] += 1
   
   index = -1
   
   while array[index] == 10
-    array[index-1] += 1
+    if array[index-1]
+      array[index-1] += 1 
+    else
+      array.unshift(1)
+    end
     array[index] = 0
     index -= 1
   end
